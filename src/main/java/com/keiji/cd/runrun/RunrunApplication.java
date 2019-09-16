88c4b7c68392d74b790f43b65dfa11967a3464b1
1278955469@qq.com <1278955469@qq.com>
2019-09-16 09:46:45 +0800

    Initial commit

---
+++ src/main/java/com/keiji/cd/runrun/RunrunApplication.java
@@ -0,0 +1,13 @@
+package com.keiji.cd.runrun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunrunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RunrunApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/keiji/cd/runrun/RunrunApplicationTests.java
@@ -0,0 +1,16 @@
+package com.keiji.cd.runrun;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RunrunApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
